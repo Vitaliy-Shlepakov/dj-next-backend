dc47a3dad108c00068ed33b481d024c17f876a41
Виталий Шлепаков <shlepakov58@gmail.com>
2023-03-31 13:33:28 +0300

    initial commit

---
+++ config/plugins.js
@@ -0,0 +1,21 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  'strapi-plugin-populate-deep': {
+    config: {
+      defaultDepth: 3, // Default is 5
+    }
+  },
+});
